3fd85093730f5a98b80c1496e0613cb53747f487
sobhanplusplus <sobhan.plus.plus@gmail.com>
2018-04-05 10:15:27 +0430

    TestRun

--- Telegrambot/Controllers/WebhookController.cs
+++ Telegrambot/Controllers/WebhookController.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using Telegram.Bot.Types;
 
 namespace Telegrambot.Controllers
 {
     public class WebhookController : ApiController
     {
-    }
+
+        Telegram.Bot.TelegramBotClient Bot;
+
+        public WebhookController()
+        {
+            Bot = new Telegram.Bot.TelegramBotClient("438518161:AAG5xVKFbV4uLf_6CtbyocQhbBv7hHLyL5A");
+        }
+        [HttpPost]
+        public async Task<IHttpActionResult> UpdateMsg(Update update)
+        {
+            try
+            {
+                if (update.Message.Text == "/start")
+                {
+                    await Bot.SendTextMessageAsync(update.Message.From.Id, "Welcome To My Bot");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+           
+            return Ok(update);
+        }
+        }
 }
--- Telegrambot/Global.asax.cs
+++ Telegrambot/Global.asax.cs
@@ -1,9 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿ 
+using System;
+ 
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Routing;
+using Telegram.Bot.Types.ReplyMarkups;
+ 
 
 namespace Telegrambot
 {
@@ -12,6 +16,11 @@ namespace Telegrambot
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+        
+            Telegram.Bot.TelegramBotClient bot = new Telegram.Bot.TelegramBotClient("438518161:AAG5xVKFbV4uLf_6CtbyocQhbBv7hHLyL5A");
+           bot.SetWebhookAsync("https://f5dbfba1.ngrok.io/api/webhook").Wait();
+           
         }
     }
 }
